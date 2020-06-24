e22a29e68dd8e65aeb3c4831c63d8efd58867bc6
Josef Fort <fort.josef@gmail.com>
2020-06-24 13:32:07 +0300

    Quaternion Look

---
+++ Assets/Scripts/Aim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+    [SerializeField]
+    private Transform _target;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 directionToFace = _target.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace);
+        //directionToFace = Quaternion.Euler(new Vector3(0, -90, -90)) * directionToFace;
+        Quaternion newRotation = Quaternion.LookRotation(directionToFace);
+        transform.rotation = newRotation;
+    }
+}
