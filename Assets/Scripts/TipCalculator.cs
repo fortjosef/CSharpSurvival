6cf51079058ea854c39fda64e9ce3c9263b73dea
Josef Fort <fort.josef@gmail.com>
2020-06-29 13:33:11 +0300

    Tip Calculator Exercise

---
+++ Assets/Scripts/TipCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalculator : MonoBehaviour {
+    public float bill = 40f;
+    public float tipPct = .2f;
+
+    // Start is called before the first frame update
+    void Start() {
+        float tip = bill * tipPct;
+        Debug.Log("Your bill is " + bill + "\nYour tip is " + tip + "\nYour total is " + (bill + tip));
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
