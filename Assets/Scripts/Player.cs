29b36f847d8b2dce7a9a057dbbb06267cfd22c58
Josef Fort <fort.josef@gmail.com>
2020-07-01 15:51:21 +0300

    change color

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public Vector3 startPosition;
+    Renderer renderer;
 
     // Start is called before the first frame update
     void Start() {
         //make sure player is at the origin
         transform.position = startPosition;
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = Color.red;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetKeyDown(KeyCode.C)) {
+            renderer.material.color = Color.green;
+        }
     }
 }
